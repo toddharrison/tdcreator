b7495ac6251d231232ef3ca0cc041aacd7f3ca8e
Todd Harrison <todd@eharrison.com>
2019-06-12 20:19:38 -0600

    Added pause to loop.

--- src/main/kotlin/com/eharrison/game/tdcreator/Loop.kt
+++ src/main/kotlin/com/eharrison/game/tdcreator/Loop.kt
@@ -2,13 +2,15 @@ package com.eharrison.game.tdcreator
 
 // https://gafferongames.com/post/fix_your_timestep/
 
+typealias Check = () -> Boolean
 typealias Input<In> = () -> In
 typealias Integrate<In, State> = (In, State, Double, Double) -> State
 typealias Interpolate<State> = (State, Double, State, Double) -> State
 typealias Render<State> = (State) -> Unit
 
 fun <In, State> loop(
-    running: () -> Boolean,
+    running: Check,
+    paused: Check,
     dst: Double,
     drt: Double,
     mit: Double, // max integration time
@@ -31,34 +33,36 @@ fun <In, State> loop(
     var renderAccumulator = 0.0
 
     while (running()) {
-        val newTime = System.nanoTime()
-        val frameTime = Math.min((newTime - currentTime).times(1.0e-9), mit)
-        currentTime = newTime
+        if (!paused()) {
+            val newTime = System.nanoTime()
+            val frameTime = Math.min((newTime - currentTime).times(1.0e-9), mit)
+            currentTime = newTime
 
-        stateAccumulator += frameTime
-        renderAccumulator += frameTime
+            stateAccumulator += frameTime
+            renderAccumulator += frameTime
 
-        if (stateAccumulator >= dst) {
-            val i = input()
-            while (stateAccumulator >= dst) {
-                previousState = currentState
-                currentState = integrate(i, currentState, t, dst)
-                t += dst
-                stateAccumulator -= dst
+            if (stateAccumulator >= dst) {
+                val i = input()
+                while (stateAccumulator >= dst) {
+                    previousState = currentState
+                    currentState = integrate(i, currentState, t, dst)
+                    t += dst
+                    stateAccumulator -= dst
+                }
             }
-        }
 
-        if (drt > 0.0) {
-            while (renderAccumulator >= drt) {
+            if (drt > 0.0) {
+                while (renderAccumulator >= drt) {
+                    val blend = stateAccumulator / dst
+                    val state = interpolate(previousState, 1.0 - blend, currentState, blend)
+                    render(state)
+                    renderAccumulator -= drt
+                }
+            } else {
                 val blend = stateAccumulator / dst
                 val state = interpolate(previousState, 1.0 - blend, currentState, blend)
                 render(state)
-                renderAccumulator -= drt
             }
-        } else {
-            val blend = stateAccumulator / dst
-            val state = interpolate(previousState, 1.0 - blend, currentState, blend)
-            render(state)
         }
     }
 }
